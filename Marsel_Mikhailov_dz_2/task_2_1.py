ce004c86b176031a06130f441be83ea1b88e187b
Marsik505 <marsik505@inbox.ru>
2022-06-22 10:16:46 +0300

    2_lessons

---
+++ Marsel_Mikhailov_dz_2/task_2_1.py
@@ -0,0 +1,15 @@
+''' Выяснить тип результата выражений:
+15 * 3
+15 / 3
+15 // 2
+15 ** 2 '''
+
+
+def type_of_object(n):
+    print(type(n))
+
+
+type_of_object(15 * 3)
+type_of_object(15 / 3)
+type_of_object(15 // 2)
+type_of_object(15 ** 2)
